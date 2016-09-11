39abbd06582641bdcb2456c832337ad7b7c679fb
Robert Stein <robert@blueshoe.de>
2016-09-11 16:07:05 +0200

    fix typo in readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Python Rest Countries
 
 
-This is a simple python wrapper for the API of [http://restcoutries.eu](http://restcoutries.eu). 
+This is a simple python wrapper for the API of [http://restcountries.eu](http://restcountries.eu). 
 If there are any issues, please use this repository to contact me about it.
  
 ## Usage
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import find_packages
 
 setup(
     name='python-restcountries',
-    version='0.1a1',
+    version='0.1a2',
     author='Robert Stein',
     author_email='robert@blueshoe.de',
     url='https://github.com/SteinRobert/python-restcountries',
-    download_url='https://github.com/SteinRobert/python-restcountries/tarball/0.1a1',
+    download_url='https://github.com/SteinRobert/python-restcountries/tarball/0.1a2',
     packages=find_packages(),
     dependencies=['future', 'requests'],
     description='Python API Wrapper for restcountries.eu',
