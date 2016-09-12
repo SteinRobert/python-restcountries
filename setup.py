18d6446f4f35980a3b509d7c2411a774f1c2c1ce
Robert Stein <robert@blueshoe.de>
2016-09-12 09:54:06 +0200

    update version

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import find_packages
 
 setup(
     name='python-restcountries',
-    version='0.1a2',
+    version='0.1a3',
     author='Robert Stein',
     author_email='robert@blueshoe.de',
     url='https://github.com/SteinRobert/python-restcountries',
-    download_url='https://github.com/SteinRobert/python-restcountries/tarball/0.1a2',
+    download_url='https://github.com/SteinRobert/python-restcountries/tarball/0.1a3',
     packages=find_packages(),
     dependencies=['future', 'requests'],
     description='Python API Wrapper for restcountries.eu',
